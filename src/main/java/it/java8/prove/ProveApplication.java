e31678ee443a9ce079745aca61e5f6dccb4ac1a8
marco.guarino <marco.guarino@maggioli.it>
2024-07-16 13:10:54 +0200

    metodo per sort di stringhe

--- src/main/java/it/java8/prove/ProveApplication.java
+++ src/main/java/it/java8/prove/ProveApplication.java
@@ -1,8 +1,11 @@
 package it.java8.prove;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
-
+import java.util.stream.Collectors;
+import static java.util.Comparator.comparing;
+import static java.util.Comparator.naturalOrder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -22,7 +25,15 @@ public class ProveApplication {
         "Firenze"
 		);
 
-		
+		System.out.println(city.stream()
+			.collect(Collectors.groupingBy(String::length)));
+
+		System.out.println(ordina(city));
+
 	}
 
+	public static List<String> ordina(List<String> input){
+		return input.stream().sorted(comparing(String::length).thenComparing(naturalOrder()))
+			.collect(Collectors.toList());
+	}
 }
