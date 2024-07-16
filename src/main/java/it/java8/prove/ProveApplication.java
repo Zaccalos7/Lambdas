06c2acf66329bb63672759d4cb926b980d2e901e
marco.guarino <marco.guarino@maggioli.it>
2024-07-16 11:04:58 +0200

    first

---
+++ src/main/java/it/java8/prove/ProveApplication.java
@@ -0,0 +1,13 @@
+package it.java8.prove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/java8/prove/ProveApplicationTests.java
@@ -0,0 +1,13 @@
+package it.java8.prove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
