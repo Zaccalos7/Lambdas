5fe1db089036dbff33403fc30fd9124ee9d9d3dc
marco.guarino <marco.guarino@maggioli.it>
2024-07-16 11:55:02 +0200

    starting project jdk17

--- src/main/java/it/java8/prove/ProveApplication.java
+++ src/main/java/it/java8/prove/ProveApplication.java
@@ -1,5 +1,8 @@
 package it.java8.prove;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +11,18 @@ public class ProveApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProveApplication.class, args);
+		
+		List<String> city = Arrays.asList(
+        "Roma",
+        "Milano",
+        "Napoli",
+        "Torino",
+        "Palermo",
+        "Genova",
+        "Firenze"
+		);
+
+		
 	}
 
 }
